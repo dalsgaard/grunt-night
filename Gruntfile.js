375143a39b6ea0e158c33c365c7f7b14ad011d42
Kim Dalsgaard <kim@kimdalsgaard.com>
2013-11-06 17:02:03 +0100

    Jade setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,23 @@
 
 module.exports = function(grunt) {
 
-  
-  
+  grunt.initConfig({
+
+    jade: {
+      devel: {
+        options: {
+          pretty: true
+        },
+        expand: true,
+        cwd: 'jade',
+        src: '*.jade',
+        ext: '.html',
+        dest: 'public'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
 }
